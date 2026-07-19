303bdf2325921bd974e49000b011738a273171b1
Yasir Arafat <yeasherarafath@gmail.com>
2026-07-19 22:07:35 +0600

    refactor: extract navbar HTML into partials
    - Extract god/owner/customer navbar to partials
    - Reorder sidebar before navbar in layouts.app
    - Add aria-label to page header

---
+++ resources/views/partials/navbar/owner.blade.php
@@ -0,0 +1,62 @@
+<header class="navbar navbar-expand-md d-none d-lg-flex d-print-none" data-bs-theme="dark">
+    <div class="container-xl">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-nav flex-row order-md-last">
+            <div class="d-none d-md-flex">
+                <div class="nav-item">
+                    <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable dark mode" title="Enable dark mode">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
+                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                        </svg>
+                    </a>
+                    <a href="?theme=light" class="nav-link px-0 hide-theme-light" data-bs-toggle="tooltip" data-bs-placement="bottom" aria-label="Enable light mode" title="Enable light mode">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
+                            <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                            <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="nav-item dropdown d-none d-md-flex">
+                    <a href="#" class="nav-link px-0" data-bs-toggle="dropdown" tabindex="-1" aria-label="Show notifications" data-bs-auto-close="outside" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
+                            <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"></path>
+                            <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
+                        </svg>
+                        <span class="badge bg-red"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-end dropdown-menu-card">
+                        <div class="card">
+                            <div class="card-header d-flex">
+                                <h3 class="card-title">Notifications</h3>
+                                <div class="btn-close ms-auto" data-bs-dismiss="dropdown"></div>
+                            </div>
+                            <div class="card-body">
+                                <div class="text-secondary">No new notifications.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link d-flex lh-1 p-0 px-2" data-bs-toggle="dropdown" aria-label="Open user menu">
+                    <span class="avatar avatar-sm bg-primary-lt">{{ initials(auth('owner')->user()->name ?? 'O') }}</span>
+                    <div class="d-none d-xl-block ps-2">
+                        <div>{{ auth('owner')->user()->name ?? 'Owner' }}</div>
+                        <div class="mt-1 small text-secondary">{{ auth('owner')->user()->business_name ?? '' }}</div>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                    <form method="POST" action="{{ route('owner.logout') }}">
+                        @csrf
+                        <button class="dropdown-item">Logout</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-menu">
+            <ul class="navbar-nav"></ul>
+        </div>
+    </div>
+</header>
